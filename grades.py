71e01d7ba2e1de1ee68eff06b8c4b7fbdb2ee960
dionysus_pc <dionysus.cho@gmail.com>
2019-05-15 19:12:29 -0400

    grades.py - update percent goal working updating received grade updates goal of other children

--- grades.py
+++ grades.py
@@ -17,37 +17,32 @@ class Grades:
     goal_grade - goal of grade as mark
     sub_grades - list of sub grades
     _parent - parent of the tree
+    _remaining_percent - percent needed for sub grades to achieve goal percent
 
     == precondition ==
         all values (except grade_received) >= 0
 
     """
-    weight: Optional[float]
+    weight: float
     grade_received: Optional[float]
     grade_total: Optional[float]
+
     goal_percent: Optional[int]
+    _remaining_percent: int
 
     _goal_grade: float
-    _needed_percent: float
     _sub_grades: List[Grades]
     _parent: Optional[Grades]
-
-    '''
-    marks received in total = sum of sub grade_received
-    grade total = 
-    
-    '''
-
 
     def __init__(self,
                  weight: Optional[float],
                  grade_total: Optional[float],
-                 goal_percent: Optional[int],
-                 parent: Optional[Grades]
+                 goal_percent: Optional[int]
                  ) -> None:
         """
         """
         self.weight = weight
+
         self.grade_received = None
 
         if grade_total is None:
@@ -56,21 +51,30 @@ class Grades:
             self.grade_total = grade_total
 
         self._sub_grades = []
-
-        self._parent = parent
-        if self._parent is not None:
-            if self not in self._parent._sub_grades:
-                self._parent._sub_grades.append(self)
+        self._parent = None
 
         if goal_percent is not None:
             self.goal_percent = goal_percent
         elif self._parent is None:
             self.goal_percent = GOAL_PERCENT
         else:
-            # TODO: update all goal percents
-            # self.goal_percent = self.get_goal_percent()
-            self.goal_percent = self._parent.goal_percent
+            self.goal_percent = self._parent._remaining_percent
+
+        self._remaining_percent = self.goal_percent
+
         self._goal_grade = math.ceil(self.goal_percent / 100 * self.grade_total)
+
+    def add_subgrade(self, grade: Grades):
+        self._sub_grades.append(grade)
+        grade._parent = self
+
+        if grade.weight is not None:
+            if self.weight is None:
+                self.weight = grade.weight
+            else:
+                self.weight += grade.weight
+
+        self.update_all_goal_percents()
 
     def update_grade_received(self, grade: Optional[float]) -> None:
         """update the grade received with a float rounded to 1 decimal point
@@ -79,39 +83,53 @@ class Grades:
         self.grade_received = grade
 
         # update all
-        self._parent.update_all_goal_percents()
+        if self._parent is not None:
+            self._parent.update_all_goal_percents()
 
     def update_goal_percent(self, goal_percent: Optional[int]) -> None:
-        """update the goal percent with goal_percent of tree and children
+        """update the goal_percent and update goal_grade
         """
         self.goal_percent = goal_percent
         self.update_goal_grade()
 
-        # update all
-        self._parent.update_all_goal_percents()
-
     def update_all_goal_percents(self) -> None:
         """update the goal percent with goal_percent of tree and children
         """
-        # if self._sub_grades == []:
-        #     pass
-        # if self._parent is None:
-        #     pass
-        # else:
-        #     self._parent
+        # TODO: but what if goal is set for a subgrade?
 
-        percent_received = 0
+        self.update_remaining_percent()
 
-        for each in self._sub_grades:
+        for sub in self._sub_grades:
+            if sub.grade_received is None:
+                sub.update_goal_percent(self._remaining_percent)
 
+    def update_remaining_percent(self) -> None:
+        """
+        for parent nodes, update remaining_percent needed for subgrades
+        to received desired grade
+        """
+        total_weight = 0
+
+        rcv_weight = 0
+        rcv_value = 0
+
+        for sub in self._sub_grades:
+
+            total_weight += sub.weight
+
+            if sub.grade_received is not None:
+                rcv_weight += sub.weight
+                rcv_value += sub.weight * sub.grade_received / sub.grade_total
+
+        self.weight = total_weight
+
+        self._remaining_percent = math.ceil(
+            (self.goal_percent / 100 * total_weight - rcv_value) /
+            (total_weight - rcv_weight) * 100
+        )
 
     def update_goal_grade(self) -> None:
         """update the goal_grade with the required grade (ceil) to
         achieve goal_percent
         """
         self._goal_grade = math.ceil(self.goal_percent / 100 * self.grade_total)
-
-    def get_goal_percent(self) -> int:
-        """return goal percent required for item to achieve goal_percent
-        """
-        pass
--- grades_test.py
+++ grades_test.py
@@ -14,31 +14,100 @@ parent: Optional[Grades]) -> None:
 def test_single_grade() -> None:
     """Test for unnested grades
     """
-    test = Grades(100, 20, 95, None)
+    test = Grades(100, 20, 95)
     assert test._goal_grade == 19
 
-    test02 = Grades(None, None, 95, None)
+    test02 = Grades(100, None, 95)
     assert test02._goal_grade == 95
+    assert test02.goal_percent == 95
 
 
-def test_tree_grade() -> None:
-    """Test for nested grades
+def test_single_percent() -> None:
+    """Test for single unnested grade
     """
-    tests = Grades(None, None, 95, None)
-    test01 = Grades(None, None, None, tests)
+    tests = Grades(None, None, 90)
+    test01 = Grades(100, None, None)
 
-    assert test01._parent is tests
-    assert test01._goal_grade == 95
+    tests.add_subgrade(test01)
+    assert test01.goal_percent == 90
 
 
-def test_tree_existing_grade() -> None:
-    """Test for nested grades
+def test_two_percent_no_rcv() -> None:
+    """Test for two unnested grades with no grades received
     """
-    tests = Grades(None, None, 90, None)
-    test01 = Grades(50, None, None, tests)
+    tests = Grades(None, None, 90)
+    test01 = Grades(50, None, None)
+    test02 = Grades(50, None, None)
+
+    tests.add_subgrade(test01)
+    tests.add_subgrade(test02)
+    assert test01.goal_percent == 90
+    assert test02.goal_percent == 90
+
+
+def test_update_remaining_percent() -> None:
+    """Test for update_remaining_percent method
+    """
+    tests = Grades(None, None, 90)
+    test01 = Grades(50, 100, None)
+    test02 = Grades(50, 100, None)
+
+    tests._sub_grades.append(test01)
+    test01._parent = tests
+    tests._sub_grades.append(test02)
+    test02._parent = tests
+
+    test02.grade_received = 80
+
+    tests.update_remaining_percent()
+
+    assert tests._remaining_percent == 100
+
+
+def test_two_percent_single_rcv() -> None:
+    """Test for two unnested grades with single grade received.
+    """
+    tests = Grades(None, None, 90)
+
+    test01 = Grades(50, 100, None)
+    test02 = Grades(50, 100, None)
+
+    tests.add_subgrade(test01)
+    tests.add_subgrade(test02)
+
+    assert test01.goal_percent == 90
+    assert test02.goal_percent == 90
+
     test01.update_grade_received(80)
-    test02 = Grades(50, None, None, tests)
-    assert test02._goal_grade == 100
+
+    assert tests._remaining_percent == 100
+    assert test02.goal_percent == 100
+
+
+# def test_tree_grade() -> None:
+#     """Test for nested grades
+#     """
+#     tests = Grades(None, None, 95, None)
+#     test01 = Grades(None, None, None, tests)
+#
+#     assert test01._parent is tests
+#     assert test01.goal_percent == 95
+#     assert test01._goal_grade == 95
+#
+#
+# def test_tree_existing_grade() -> None:
+#     """Test for nested grades
+#     """
+#     tests = Grades(None, None, 90)
+#     test01 = Grades(50, None, None)
+#     test02 = Grades(50, None, None)
+#
+#     assert test01.goal_percent == 90
+#     assert test02.goal_percent == 90
+#
+#     test01.update_grade_received(80)
+#
+#     assert test02._goal_grade == 100
 
 
 if __name__ == '__main__':
